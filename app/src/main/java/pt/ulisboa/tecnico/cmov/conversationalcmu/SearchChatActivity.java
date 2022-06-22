c7112f0cc170901c9bb06233e4681af7fb27cda3
PedroTheAxe <morais.pedro2000@gmail.com>
2022-06-22 01:48:10 +0100

    Geofenced done

--- app/src/main/java/pt/ulisboa/tecnico/cmov/conversationalcmu/SearchChatActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/conversationalcmu/SearchChatActivity.java
@@ -1,15 +1,25 @@
 package pt.ulisboa.tecnico.cmov.conversationalcmu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -100,11 +110,37 @@ public class SearchChatActivity extends AppCompatActivity {
                 JSONArray jsonArray = new JSONArray(response.body().string());
                 for ( int i = 0; i < jsonArray.length(); i++) {
                     JSONObject entry = jsonArray.getJSONObject(i);
-                    chatroomsList.add(entry);
+                    if (entry.getString("chatType").equals("Geo-Fenced")) {
+                        if (checkIfInGeoFence(entry)) {
+                            chatroomsList.add(entry);
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        chatroomsList.add(entry);
+                    }
                 }
             } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
         }).start();
     }
+
+    private boolean checkIfInGeoFence(JSONObject entry) throws JSONException {
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        Location locationB = new Location("Chat center");
+        locationB.setLatitude(Double.parseDouble(entry.getString("chatLat")));
+        locationB.setLongitude(Double.parseDouble(entry.getString("chatLon")));
+        Log.e("fixe", String.valueOf(location.distanceTo(locationB)));
+        Log.e("fixe", String.valueOf((Double.parseDouble(entry.getString("chatRadius")) * 1000)));
+        if ((Double.parseDouble(entry.getString("chatRadius")) * 1000) >= location.distanceTo(locationB)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
